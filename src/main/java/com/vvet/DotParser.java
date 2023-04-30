c3e55c98ae3ad3456b6b4252e65c708392b0a5ed
Gabriel Limoni <gabriel.limoni@liven.tech>
2023-04-30 17:20:28 -0300

    feat: graph writer

--- src/main/java/com/vvet/DotParser.java
+++ src/main/java/com/vvet/DotParser.java
@@ -1,13 +1,10 @@
 package com.vvet;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-import guru.nidi.graphviz.engine.Format;
-import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.MutableGraph;
 import guru.nidi.graphviz.parse.Parser;;
 
@@ -15,7 +12,6 @@ public class DotParser {
   static MutableGraph parse(String path) throws FileNotFoundException, IOException {
     InputStream dot = new FileInputStream(path);
     MutableGraph g = new Parser().read(dot);
-    Graphviz.fromGraph(g).width(700).render(Format.PNG).toFile(new File("example/ex4-1.png"));
     return g;
   }
 }
---
+++ src/main/java/com/vvet/WriteGraph.java
@@ -0,0 +1,14 @@
+package com.vvet;
+
+import java.io.File;
+import java.io.IOException;
+
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.MutableGraph;
+
+public class WriteGraph {
+  static void write(MutableGraph g, String path) throws IOException {
+    Graphviz.fromGraph(g).width(700).render(Format.PNG).toFile(new File(path));
+  }
+}
